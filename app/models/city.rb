7ac2d3698fda3ce0f6f638b45073b69adaf063c8
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-25 11:15:28 -0600

    added validation for email

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,11 @@
 class City < ActiveRecord::Base
+  VALID_STATES = %w(ID WA OR)
+  validates :elevation,
+            numericality: {
+              greater_than_or_equal_to: 3315,
+              if: lambda { |city| city.state == 'CO' },
+              message: 'is too low, the lowest elevation in Colorado is 3,315 feet'
+            }
+   validates :name, presence: { message: "Go away" }
+
 end
