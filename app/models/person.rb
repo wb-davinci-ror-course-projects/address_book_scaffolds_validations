117274a3db3c711c99ca93249ac1860959269252
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-25 11:51:00 -0600

    added validations for zipcode

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ActiveRecord::Base
+  # is there something that can take place of following for all fields
+  validates :first_name, :last_name, :address_line_one, :city, :state, :zip_code,  presence: true
+  validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
+  validates :zip_code, length: { in: 5..10 }
+
 end
